a747e965add4128d388de0d027e10de65638c6cf
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-09-23 01:26:07 +0530

    add redux folder

---
+++ redux/src/store/index.js
@@ -0,0 +1,12 @@
+// global store...where we will be storing all of our components state
+
+// redux toolkit is used for the state management in raect and to overcome the propblem of prop drilling
+// things that are needed to create our redux store are ->
+// 1. reducer(reducer function)
+// 2. state(initial state)
+// 3. store(store function from redux library)
+// 4 .slice
+// 5 action
+
+
+// Example ->
